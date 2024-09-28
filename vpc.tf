4f68720d1af466d74c9111daec5b222a1b0660ce
SrivalliAvala <srivalliavala999@gmail.com>
2024-09-28 13:54:47 +0530

    terraform vpc

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+    source = "C:/devops/daws-81s/repos/terraform/terraform-adv/terraform-aws-vpc"
+    project_name = var.project_name
+    Environment = var.Environment
+    common_tags = var.common_tags
+    vpc_cidr = var.vpc_cidr
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+
+
+}
