cbfcbcfb2eaffb325951fd8f660be8abaad226d6
SrivalliAvala <srivalliavala999@gmail.com>
2024-10-03 14:20:23 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "project_name" {
     default = "expense"  
 }
 
-variable "Environment" {
+variable "environment" {
     default = "dev"
 }
 
@@ -14,18 +14,22 @@ variable "common_tags" {
     }
 }
 
-variable "vpc_cidr" {
-    default = "10.0.0.0/16"
-}
+# variable "vpc_cidr" {
+#     default = "10.0.0.0/16"
+# }
 
-variable "public_subnet_cidrs" {
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
+# variable "public_subnet_cidrs" {
+#     default = ["10.0.1.0/24", "10.0.2.0/24"]
+# }
 
-variable "private_subnet_cidrs" {
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
-}
+# variable "private_subnet_cidrs" {
+#     default = ["10.0.11.0/24", "10.0.12.0/24"]
+# }
 
-variable "database_subnet_cidrs" {
-    default = ["10.0.21.0/24", "10.0.22.0/24"]
+# variable "database_subnet_cidrs" {
+#     default = ["10.0.21.0/24", "10.0.22.0/24"]
+# }
+
+variable "is_peering_required" {
+    default = true
 }
